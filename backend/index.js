993b389370391c05a51b25a41931cae2460a70cb
Anton Davydov <fetsorn@gmail.com>
2022-01-09 09:58:10 +0300

    build (backend): remove comments

--- backend/index.js
+++ backend/index.js
@@ -9,17 +9,10 @@ const app = express();
 
 app.use((req, res, next) => {
     if (/static/i.test(req.path)) {
-        // console.log("match static", req.path)
         next()
     } else if (/api\/(hosts|assets)/i.test(req.path)) {
-        // console.log("match hosts/assets", req.path)
-        // var trim = req.path.replace(/^\/api/, "")
-        // console.log(trim)
-        // var full = path.join(process.cwd(), trim)
-        // console.log(full)
         res.sendFile(path.join(process.cwd(), req.path.replace(/^\/api/, "")));
     } else {
-        // console.log("match default", req.path)
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     }
 });

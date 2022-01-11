8fe3618432a5e6f241a7a4663a71a0b7df4d6fc2
Anton Davydov <fetsorn@gmail.com>
2022-01-11 16:40:37 +0300

    frontend: remove header and footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div className={styles.title}>&lt;/конец&gt;</div>
+    <div className={styles.title}></div>
   </footer>
 )
 
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css'
 
 const Header = () => (
   <header className={styles.header}>
-    <h1 className={styles.title}>Таймлайн</h1>
+    <h1 className={styles.title}></h1>
   </header>
 )
 

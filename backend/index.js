5face3c66d157626cac2f6c599e1392dc4268411
Anton Davydov <fetsorn@gmail.com>
2022-01-12 00:01:31 +0300

    backend: add overview page and comments

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,23 @@ const fs = require('fs');
 const app = express();
 
 app.use((req, res, next) => {
-    if (/static/i.test(req.path)) {
+    // on / serve a custom overview page in current directory
+    if (/^\/$/i.test(req.path)) {
+      res.sendFile(path.join(process.cwd(), "index.html"));
+    // on /static skip to serving /build/static
+    } else if (/static/i.test(req.path)) {
         next()
+    // on /api/assets and /api/hosts, serve assets/ and hosts/ in current directory
     } else if (/api\/(hosts|assets)/i.test(req.path)) {
         res.sendFile(path.join(process.cwd(), req.path.replace(/^\/api/, "").replace(/%20/g, "\ ")));
+    // in other cases serve a react app that routes urls to /api/assets/ and /api/hosts/
     } else {
+        console.log(req.path)
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     }
 });
 
+// serve build/ at /, allows to serve build/static at static/
 app.use(express.static(path.join(__dirname, 'build')));
 
 const PORT = process.env.PORT || 8080;

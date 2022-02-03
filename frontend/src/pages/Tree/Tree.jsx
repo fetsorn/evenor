0aebda43035678d501f52ade8024f8ca627e5557
Anton Davydov <fetsorn@gmail.com>
2022-02-03 07:49:13 +0300

    change root & depth from UI #22

--- frontend/src/pages/Tree/Tree.jsx
+++ frontend/src/pages/Tree/Tree.jsx
@@ -1,8 +1,8 @@
 import styles from './Tree.module.css'
 
-import { useEffect, useState, useMemo } from 'react'
-import { Header } from '@components'
-import { ged2dot } from '@utils'
+import { useEffect, useState, useMemo, createRef } from 'react'
+import { Header, Button, StringToJSX } from '@components'
+import { ged2dot, ged2dot_ } from '@utils'
 
 import http from 'isomorphic-git/http/web'
 import LightningFS from '@isomorphic-git/lightning-fs'
@@ -37,29 +37,35 @@ async function fetchData() {
   }
 }
 
-async function transformData(restext) {
-  // convert gedcom to dot notation
-  var dot = ged2dot(restext)
-  // console.log(dot)
+async function dot2svg(dot) {
 
   // render dot notation with graphviz
   var hpccWasm = window["@hpcc-js/wasm"];
   var svg = await hpccWasm.graphviz.layout(dot, "svg", "dot")
-  // console.log(svg)
 
   return svg
 }
 
 
 const Tree = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState("")
   const [dataLoading, setDataLoading] = useState([])
+  const [svg, setSvg] = useState("")
+  const [depth, setDepth] = useState(4)
+  const rootInput = createRef()
+
+  const render = async () => {
+    var root = rootInput.current.value
+    var dot = ged2dot(data, root, depth)
+    setSvg(await dot2svg(dot))
+  }
 
   useEffect( () => {
     async function setTree() {
       var restext = await fetchData()
-      var svg = await transformData(restext)
-      setData(svg)
+      setData(restext)
+      var dot = ged2dot_(restext)
+      setSvg(await dot2svg(dot))
     }
     setTree()
     setDataLoading(false)
@@ -68,7 +74,15 @@ const Tree = () => {
   return (
     <>
       <Header />
-      <div className={styles.container} dangerouslySetInnerHTML={{ __html: data }}></div>
+      <input type="text" ref={rootInput} id="rootInput" value="F0001" onChange={(e) => {
+        render()
+      }}/>
+      <div>Depth: {depth}</div>
+      <input type="range" min="1" max="10" value={depth} onChange={(e) => {
+        setDepth(e.target.value)
+        render()
+      }}/>
+      <div className={styles.container} dangerouslySetInnerHTML={{ __html: svg }}></div>
     </>
   )
 }

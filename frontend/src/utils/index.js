0cbdc5a2de6477b4380f9ed0a38786551466fec5
Anton Davydov <fetsorn@gmail.com>
2022-02-22 00:26:41 +0300

    move metadir function to utils

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,2 +1,3 @@
 export { default as formatDate } from './formatDate'
 export { ged2dot, ged2dot_ } from './ged2dot'
+export { queryMetadir, resolveAssetPath, queryMetadirEvea, addEvent, editEvent, deleteEvent, commit } from './tbn'

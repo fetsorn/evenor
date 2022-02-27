77c7b408b9bc6d674a6e354555d0da41df338b98
Anton Davydov <fetsorn@gmail.com>
2022-02-28 01:57:17 +0300

    test and refactor deleteEvent

--- frontend/src/utils/tbn.spec.js
+++ frontend/src/utils/tbn.spec.js
@@ -1,6 +1,6 @@
 import LightningFS from '@isomorphic-git/lightning-fs';
 import http from 'isomorphic-git/http/web'
-import { queryMetadir, editEvent } from '@utils'
+import { queryMetadir, editEvent, deleteEvent } from '@utils'
 const tbn2 = require('@utils/tbn2')
 import { TextEncoder, TextDecoder } from 'util'
 import crypto from 'crypto'
@@ -186,6 +186,32 @@ f35d45c3ee3e68cf9e36ee10df3edb02104c22b2d47ab17e64114ffb9c208265,b218ca013905fc5
 c55581aff06024b65866642ed14f73a6f0e555821f3366fd8f10d74570fac920,8b30955ad81009092a766bab12ede073956eb5ef1862f2ab5ac5b69ab43a79c5
 `
 
+var csvMockNo3 = { ...csvMock }
+
+csvMockNo3["metadir/pairs/datum-guestname.csv"] = `8260502525153a8775ecb052f41e4e908aba4c94b07ef90263fff77195392704,9367417d63903350aeb7e092bca792263d4fd82d4912252e014e073a8931b4c1
+b52dc2b8884fc396c108c095da157d8607ee7d61a1e6b4b501b660d42f93c58e,069587dcb8f8b63329ae53051ba79ba34ba0deb41c7a1e044280d7b6bb15e4f0
+`
+
+csvMockNo3["metadir/pairs/datum-hostname.csv"] = `8260502525153a8775ecb052f41e4e908aba4c94b07ef90263fff77195392704,9367417d63903350aeb7e092bca792263d4fd82d4912252e014e073a8931b4c1
+b52dc2b8884fc396c108c095da157d8607ee7d61a1e6b4b501b660d42f93c58e,069587dcb8f8b63329ae53051ba79ba34ba0deb41c7a1e044280d7b6bb15e4f0
+`
+
+csvMockNo3["metadir/pairs/datum-guestdate.csv"] = `8260502525153a8775ecb052f41e4e908aba4c94b07ef90263fff77195392704,4935b73812dd87780ee8deae03d0bbcb125bbcdc05271066ca527ab029e4e79d
+b52dc2b8884fc396c108c095da157d8607ee7d61a1e6b4b501b660d42f93c58e,161c6b3d37ba3341b7775b10730b2ded837c3d84d77fb1a046fa198e9db8cbbc
+`
+
+csvMockNo3["metadir/pairs/datum-hostdate.csv"] = `8260502525153a8775ecb052f41e4e908aba4c94b07ef90263fff77195392704,4935b73812dd87780ee8deae03d0bbcb125bbcdc05271066ca527ab029e4e79d
+b52dc2b8884fc396c108c095da157d8607ee7d61a1e6b4b501b660d42f93c58e,161c6b3d37ba3341b7775b10730b2ded837c3d84d77fb1a046fa198e9db8cbbc
+`
+
+csvMockNo3["metadir/pairs/datum-filepath.csv"] = `8260502525153a8775ecb052f41e4e908aba4c94b07ef90263fff77195392704,01f8dafeb2559c983006156763f9c3b951b64688b3b41a9e5ad7cb695110e8ee
+b52dc2b8884fc396c108c095da157d8607ee7d61a1e6b4b501b660d42f93c58e,424bd3271c0c940304ec6e9f4412a422735caeeb9638038bf509e36ae5d4f865
+`
+
+csvMockNo3["metadir/props/datum/index.csv"] = `8260502525153a8775ecb052f41e4e908aba4c94b07ef90263fff77195392704,"value1"
+b52dc2b8884fc396c108c095da157d8607ee7d61a1e6b4b501b660d42f93c58e,"value2"
+`
+
 async function fetchDataMetadirMock(path, fs) {
   return csvMock[path]
 }
@@ -229,8 +255,7 @@ describe('queryMetadir', () => {
   });
 })
 
-
-describe('updateMetadir', () => {
+describe('editEvent', () => {
 
   let csvMockNew
   let fs
@@ -260,6 +285,27 @@ describe('updateMetadir', () => {
     return editEvent(event4edit, fs, dir)
       .then(() => {
         expect(csvMockNew).toStrictEqual(csvMock4)
+      });
+  });
+})
+
+describe('deleteEvent', () => {
+
+  let csvMockNew
+  let fs
+
+  beforeEach(() => {
+    csvMockNew = { ...csvMock }
+    async function writeFileMock(path, contents, encoding) {
+      csvMockNew[path] = contents
+    }
+    fs = { "promises": { "writeFile": jest.fn(writeFileMock) }}
+  })
+
+  test('deletes event', () => {
+    return deleteEvent(event3.UUID, fs, dir)
+      .then(() => {
+        expect(csvMockNew).toStrictEqual(csvMockNo3)
       });
   });
 })

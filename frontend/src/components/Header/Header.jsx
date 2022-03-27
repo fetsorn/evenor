a38bc072e4dd1555aeae26e427af5adcaef8d25a
Anton Davydov <fetsorn@gmail.com>
2022-03-27 12:37:26 +0400

    add event on "New event"

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,6 +3,8 @@ import styles from './Header.module.css'
 
 import LightningFS from '@isomorphic-git/lightning-fs';
 
+import { editEvent, commit } from '@fetsorn/csvs-js/src/tbn'
+
 const Header = ({isEdit, setIsEdit, setEvent}) => {
 
   // remove credentials to sessionStorage, wipe fs and reload
@@ -22,13 +24,20 @@ const Header = ({isEdit, setIsEdit, setEvent}) => {
   <header className={styles.header}>
     <h1 className={styles.title}></h1>
     <Button type="button" onClick={logout}>Logout</Button>
+    <Button type="button" onClick={() => {
+      let token = window.sessionStorage.getItem('token')
+      let ref = window.sessionStorage.getItem('ref')
+      commit(window.fs, window.dir, token, ref)
+    }}>Commit</Button>
+    {isEdit && (
+      <Button type="button" onClick={async () => {
+        setEvent(await editEvent({}, window.fs.promises, window.dir))
+      }}>New event</Button>
+    )}
     {setIsEdit && (
       <label>edit:
         <input type="checkbox" checked={isEdit} onChange={(e) => {console.log(isEdit, e.target.checked); setIsEdit(e.target.checked)}} />
       </label>
-    )}
-    {isEdit && (
-      <Button type="button" onClick={() => setEvent({})}>New event</Button>
     )}
   </header>
   )}

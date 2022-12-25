f530787601895285bc3f315c3a5f8fbf692a0656
fetsorn <fetsorn@gmail.com>
2022-12-25 07:17:56 +0400

    clear page tsx and folders

---
+++ src/renderer/pages/Tree/components/TreeGraph.tsx
@@ -0,0 +1,8 @@
+
+import Draggable from "react-draggable";
+      <Draggable cancel=".node">
+        <div
+          className={styles.csv}
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div>
+      </Draggable>

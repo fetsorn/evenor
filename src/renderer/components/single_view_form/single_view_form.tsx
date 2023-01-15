1370f334f2871bf94184cc418f19a6db825fcd95
fetsorn <fetsorn@gmail.com>
2023-01-15 15:49:03 +0400

    refactor single view form

--- src/renderer/components/single_view_form/single_view_form.tsx
+++ src/renderer/components/single_view_form/single_view_form.tsx
@@ -1,10 +1,5 @@
 import React, { useMemo } from "react";
-import {
-  FormOutputParagraph,
-  FormOutputArray,
-  FormOutputObject,
-  getDescription,
-} from "..";
+import { FormOutput } from "..";
 
 interface ISingleViewFormProps {
   schema: any;
@@ -19,28 +14,11 @@ export default function SingleViewForm({
 
   return (
     <div>
-      {addedFields.map((label: string, index: any) => {
-        const prop =
-          Object.keys(schema).find((p) => schema[p].label === label) ?? label;
-
-        const propType = schema[prop]?.type;
-
-        const description = getDescription(schema, label);
-
-        const value = entry[label];
-
-        return (
-          <div key={index}>
-            {propType === "array" ? (
-              <FormOutputArray {...{ schema, description, value }} />
-            ) : propType === "object" ? (
-              <FormOutputObject {...{ schema, description, value }} />
-            ) : (
-              <FormOutputParagraph {...{ schema, description, value }} />
-            )}
-          </div>
-        );
-      })}
+      {addedFields.map((label: string, index: any) => (
+        <div key={index}>
+          <FormOutput {...{ schema, label }} value={entry[label]} />
+        </div>
+      ))}
     </div>
   );
 }

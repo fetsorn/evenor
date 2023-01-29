a649f6aceb4f03d84918d7f0ec97c50b97342532
fetsorn <fetsorn@gmail.com>
2023-01-29 12:32:21 +0400

    merge search bar components

--- src/renderer/layout/header/components/header_filter/components/index.ts
+++ src/renderer/layout/header/components/header_filter/components/index.ts
@@ -1,5 +1,3 @@
 export { default as FilterQueryList } from "./filter_query_list";
 export { default as FilterSearchBar } from "./filter_search_bar";
-export { default as SearchBarDropdown } from "./search_bar_dropdown";
-export { default as SearchBarForm } from "./search_bar_form";
 export { default as QueryListLabel } from "./query_list_label";

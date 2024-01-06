ec941d49658ea97a50b52a85e13536e7219e2b4d
NasimMora <brs.team.dota@gmail.com>
2024-01-06 17:00:58 +0400

    refactor and add comments to filter_query_plus

--- src/renderer/layout/header/components/header_filter/components/filter_query_plus/filter_query_plus.jsx
+++ src/renderer/layout/header/components/header_filter/components/filter_query_plus/filter_query_plus.jsx
@@ -21,12 +21,14 @@ export function FilterQueryPlus({
     state.onQueryAdd,
   ]);
 	
-
-const addedField = Object.keys(queries)
-
-const menuItems = Object.keys(schema)
-  .filter(key => !addedField.includes(key)) 
-  .map(key => ({
+	// find all fields name
+	const schemaKeys = Object.keys(schema)
+	// find field name which added to filterqueries
+	const addedFields = Object.keys(queries)
+	// find name fields which is not added to filterqueries
+	const notAddedFields = schemaKeys.filter(key => !addedFields.includes(key))
+	// transform list of fieldnames(array of strings) to list of objects in dropdown
+	const menuItems = notAddedFields.map(key => ({
     onClick: () => {
       onQueryAdd(key, "");
     },

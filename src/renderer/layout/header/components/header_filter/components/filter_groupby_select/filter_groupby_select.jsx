90dc8909fdb2ecbceb72468b07c537cc6d572f0c
NasimMora <brs.team.dota@gmail.com>
2024-01-08 18:15:09 +0400

    Create function foo to get leaves of base to groupBy

--- src/renderer/layout/header/components/header_filter/components/filter_groupby_select/filter_groupby_select.jsx
+++ src/renderer/layout/header/components/header_filter/components/filter_groupby_select/filter_groupby_select.jsx
@@ -12,14 +12,39 @@ export function FilterGroupBySelect({
   const [
 		groupBy,
     schema,
+		base,
 		setGroupBy,
   ] = useStore((state) => [
 		state.groupBy,
     state.schema,
+		state.base,
 		state.setGroupBy,
   ]);
+
+	function foo(schema, base) {
+		console.log(`filter_query_plus: foo-base-${base}`);
+		// when base undefined then return empty array
+		// if (base === undefined) {
+		// 	return []
+		// }
+		// pass to fields of schema and find fields where trunk == base
+		// return [leaf, leaf, leaf ]
+		//когда на сайте в base выбран reponame, плюс должен возвращать schema, category, tags
+		// if (base === "reponame") {
+		// 	return ["schema", "category", "tags"]
+		// }
+		//когда на сайте в base выбран tags, плюс должен возвращать sync_tag, remote_tag, rss_tag, local_tag, zip_tag, tg_tag
+		// if (base === "tags") {
+		// 	return ["sync_tag", "remote_tag", "rss_tag", "local_tag","zip_tag", "tg_tag"]
+		// }
+		// как найти все листочки base. Когда на сайте выбран base плюс должен возвращать все ветки у которых trunk === base.
+		return Object.keys(schema).filter((branch) => schema[branch].trunk === base)
+		
+	}
+	const options = foo(schema, base)
 	
-const options = Object.keys(schema)
+	
+// const options = Object.keys(schema)
 console.log(schema);
 
   return (

03d5c2575d2135e1c2c14452cb20fc6c772fcd99
Anton Davydov <fetsorn@gmail.com>
2025-03-23 14:33:37 +0300

    add scroll to top and number of records to filter

--- src/layout/overview/components/overview_filter/overview_filter.jsx
+++ src/layout/overview/components/overview_filter/overview_filter.jsx
@@ -74,6 +74,10 @@ export function OverviewFilter() {
           )}
         </For>
       </Spoiler>
+      <span> </span>
+      <span>found {store.records.length}</span>
+      <span> </span>
+      <a onClick={() => window.scrollTo(0, 0)}>scroll to top</a>
     </span>
   );
 }

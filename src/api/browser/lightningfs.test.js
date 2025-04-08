4ee735da5be51bf37acc3218ec32853c27d7fca5
Anton Davydov <fetsorn@gmail.com>
2025-04-08 12:27:30 +0300

    test lightningfs

--- src/api/browser/lightningfs.test.js
+++ src/api/browser/lightningfs.test.js
@@ -1,19 +1,124 @@
-import { expect, test, describe, beforeAll } from "vitest";
-import { page, userEvent } from "@vitest/browser/context";
-import browser from "./index.js";
+import { expect, test, describe, beforeEach, afterEach, vi } from "vitest";
+import {
+  fs,
+  createReadStream,
+  createWriteStream,
+  mkdtemp,
+  appendFile,
+} from "./lightningfs.js";
+import stub from "./stub.js";
 
-test("createReadStream", async () => {
-  expect(false).toBe(true);
+describe("createReadStream", () => {
+  beforeEach(() => {
+    fs.init("test", { wipe: true });
+  });
+
+  afterEach(async () => {
+    // for lightning fs to release mutex on indexedDB
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  });
+
+  test("reads file", async () => {
+    await fs.promises.mkdir(stub.dirpath);
+
+    // create file
+    await fs.promises.writeFile(stub.filepath, stub.content);
+
+    // consume the stream to string
+    const strm = createReadStream(stub.filepath);
+
+    let content = "";
+
+    await strm.pipeTo(
+      new WritableStream({
+        write(character) {
+          content += character;
+        },
+      }),
+    );
+
+    // check that string equals contents
+    expect(content).toBe(stub.content);
+  });
 });
 
-test("createWriteStream", async () => {
-  expect(false).toBe(true);
+describe("createWriteStream", () => {
+  beforeEach(() => {
+    fs.init("test", { wipe: true });
+  });
+
+  afterEach(async () => {
+    // for lightning fs to release mutex on indexedDB
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  });
+
+  test("writes file", async () => {
+    await fs.promises.mkdir(stub.dirpath);
+
+    // create a read stream from string
+    const strm = ReadableStream.from(stub.content);
+
+    // pipe to the write stream
+    await strm.pipeTo(createWriteStream(stub.filepath));
+
+    // read file
+    const content = await fs.promises.readFile(stub.filepath, "utf8");
+
+    // check that contents equal string
+    expect(content).toBe(stub.content);
+  });
 });
 
-test("mkdtemp", async () => {
-  expect(false).toBe(true);
+describe("mkdtemp", () => {
+  beforeEach(() => {
+    fs.init("test", { wipe: true });
+  });
+
+  afterEach(async () => {
+    // for lightning fs to release mutex on indexedDB
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  });
+
+  test("creates a directory", async () => {
+    // mock math.random
+    Math.random = vi.fn(() => 0.0001);
+
+    await mkdtemp("/");
+
+    const listing = await fs.promises.readdir("/");
+
+    // check that tmpdir is created
+    expect(listing).toEqual(["1"]);
+  });
 });
 
-test("appendFile", async () => {
-  expect(false).toBe(true);
+describe("appendFile", () => {
+  beforeEach(() => {
+    fs.init("test", { wipe: true });
+  });
+
+  afterEach(async () => {
+    // for lightning fs to release mutex on indexedDB
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  });
+
+  test("writes to the file", async () => {
+    await fs.promises.mkdir(stub.dirpath);
+
+    // create file
+    await fs.promises.writeFile(stub.filepath, stub.content);
+
+    const content = await fs.promises.readFile(stub.filepath, "utf8");
+
+    // check file contents
+    expect(content).toBe(stub.content);
+
+    // append
+    await appendFile(stub.filepath, "-");
+
+    const contentNew = await fs.promises.readFile(stub.filepath, "utf8");
+
+    // check file contents
+    expect(contentNew).toBe(`${stub.content}-`);
+  });
 });

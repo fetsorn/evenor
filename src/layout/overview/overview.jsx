58d9b7ff17fbf02a664dc903a9c6b60f2ba4e9c7
Anton Davydov <fetsorn@gmail.com>
2025-08-02 23:01:03 +0300

    remove @tanstack/solid-virtual

--- src/layout/overview/overview.jsx
+++ src/layout/overview/overview.jsx
@@ -1,4 +1,3 @@
-import { createVirtualizer } from "@tanstack/solid-virtual";
 import { useContext } from "solid-js";
 import { StoreContext } from "@/store/index.js";
 import { getSortedRecords } from "@/store/index.js";
@@ -10,69 +9,20 @@ export function Overview() {
 
   let parentRef;
 
-  const virtualize = () =>
-    createVirtualizer({
-      get count() {
-        // here just to trigger recount on sort change
-        const searchParams = store.searchParams;
-
-        return store.records.length;
-      },
-      getScrollElement: () => parentRef,
-      estimateSize: () => 35,
-      overscan: 5,
-    });
-
-  const virtualizer = virtualize();
-
   return (
     <div ref={parentRef} className={styles.overview}>
       <h1>Entries</h1>
 
-      <div
-        className={styles.foo}
-        style={{
-          height: `${virtualize().getTotalSize()}px`,
-        }}
-      >
+      <div className={styles.foo}>
         <For
-          each={(() => {
-            // here just to trigger render on sort change
-            const searchParams = store.searchParams;
-
-            // for some reason we need this for test to pass
-            virtualizer.getVirtualItems().toString();
-
-            // need to call virtualize each time to re-render on store.records = []
-            return virtualize().getVirtualItems();
-          })()}
+          each={store.records}
           fallback={
             <span>press "new" in the top right corner to add entries</span>
           }
         >
-          {(virtualRow) => (
-            <div
-              className={styles.bar}
-              style={{
-                height: `${virtualRow.size}px`,
-                transform: `translateY(${virtualRow.start}px)`,
-              }}
-            >
-              <div
-                key={virtualRow.key}
-                data-index={virtualRow.index}
-                ref={(node) => {
-                  // https://github.com/TanStack/virtual/issues/930
-                  node.dataset.index = virtualRow.index.toString();
-
-                  virtualize().measureElement(node);
-                }}
-              >
-                <OverviewItem
-                  index={virtualRow.key}
-                  item={getSortedRecords(virtualRow.index)}
-                />
-              </div>
+          {(item, index) => (
+            <div className={styles.bar}>
+              <OverviewItem index={`overview_item_${index}`} item={item} />
             </div>
           )}
         </For>

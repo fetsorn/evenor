81af719c3c0d5bb984570b882ad21460de7ba1c1
Anton Davydov <fetsorn@gmail.com>
2025-08-07 23:51:59 +0300

    add separator

--- src/layout/overview/components/overview_item/overview_item.jsx
+++ src/layout/overview/components/overview_item/overview_item.jsx
@@ -9,6 +9,7 @@ import {
 } from "@/store/index.js";
 import { Confirmation, Spoiler } from "@/layout/components/index.js";
 import { OverviewRecord } from "../index.js";
+import styles from "./overview_item.module.css";
 
 export function OverviewItem(props) {
   const { store } = useContext(StoreContext);
@@ -20,46 +21,50 @@ export function OverviewItem(props) {
   const canOpenMind = isHomeScreen && isMind;
 
   return (
-    <>
+    <div className={styles.item}>
+      <div>
       <OverviewRecord
         index={props.index}
         record={props.item}
         isOpenDefault={true}
       />
+      </div>
 
-      <button
-        className={"edit"}
-        onClick={() =>
-          onRecordEdit(["record"], JSON.parse(JSON.stringify(props.item)))
-        }
-      >
-        edit{" "}
-      </button>
-
-      <Confirmation
-        action={`delete`}
-        question={"really delete?"}
-        onAction={() => onRecordWipe(props.item)}
-      />
-
-      <Show when={canOpenMind} fallback={<></>}>
-        <button title="zip" onClick={() => onZip(props.item.mind)}>
-          Zip{" "}
-        </button>
-      </Show>
-
-      <Show when={canOpenMind} fallback={<></>}>
+      <div className={styles.actions}>
         <button
-          title="open"
-          onClick={async () => {
-            const base = await getDefaultBase(props.item.mind);
-
-            await onMindChange(`/${props.item.mind}`, `_=${base}`)
-          }}
+          className={"edit"}
+          onClick={() =>
+            onRecordEdit(["record"], JSON.parse(JSON.stringify(props.item)))
+          }
         >
-          Open{" "}
+          edit{" "}
         </button>
-      </Show>
-    </>
+
+        <Confirmation
+          action={`delete`}
+          question={"really delete?"}
+          onAction={() => onRecordWipe(props.item)}
+        />
+
+        <Show when={canOpenMind} fallback={<></>}>
+          <button title="zip" onClick={() => onZip(props.item.mind)}>
+            Zip{" "}
+          </button>
+        </Show>
+
+        <Show when={canOpenMind} fallback={<></>}>
+          <button
+            title="open"
+            onClick={async () => {
+              const base = await getDefaultBase(props.item.mind);
+
+              await onMindChange(`/${props.item.mind}`, `_=${base}`)
+            }}
+          >
+            Open{" "}
+          </button>
+        </Show>
+      </div>
+    </div>
   );
 }
